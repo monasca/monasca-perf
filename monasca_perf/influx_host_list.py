626e8a8ef82f1a64013854a0c64cf2fff599fcae
Tim Kuhlman <tim.kuhlman@hp.com>
2014-11-25 15:18:28 -0700

    Cleaned up the influx_host_list script, ignore pycharm

--- monasca_perf/influx_host_list.py
+++ monasca_perf/influx_host_list.py
@@ -1,56 +1,41 @@
-import simplejson as json
-import urllib, sys
-import re
-import base64
+#!/usr/bin/env python
+from collections import defaultdict
+import sys
 from influxdb import client as influxdb
 
 username = sys.argv[1]
 password = sys.argv[2]
-url = sys.argv[3]
+hostname = sys.argv[3]
 
-db = influxdb.InfluxDBClient(url, 8086, username, password, 'mon')
-series_list = db.query('list series;')
+db = influxdb.InfluxDBClient(hostname, 8086, username, password, 'mon')
 
 hosts = set()
-bad_series = 0
-hosts_amplified = set()
-for series in series_list[0]["points"]:
-    series_name = series[1]
-    series_items = {}
-    series_split = re.split(r'&',series_name)
-    for series_name_item in series_split:
-        series_name_item_split = re.split(r'=',series_name_item)
-        try:
-            series_items[series_name_item_split[0]] = series_name_item_split[1]
-        except IndexError:
-            bad_series += 1
-    hostname = ""
-    try:
-        hostname = series_items['hostname']
-    except KeyError:
-        try:
-            hostname = series_items['instance_id']
-        except KeyError:
-            print series_name
-            continue
-    if len(hostname) == 0:
-        print series_name
-        continue
-    amplifier = ""
-    try:
-        amplifier = series_items['amplifier']
-    except KeyError:
-        pass
-    hostname_amplified = hostname
-    if len(amplifier) > 0:
-        hostname_amplified += ':amplifier:'
-        hostname_amplified += amplifier
-    hosts.add(hostname)
-    hosts_amplified.add(hostname)
-    hosts_amplified.add(hostname_amplified)
+hosts_amplified = defaultdict(int)
+total_metrics = 0
+metrics_missing_hostname = 0
+for series in db.query('list series;')[0]["points"]:
+    total_metrics += 1
+    entries = [entry for entry in series[1].split('&')]
+    series_items = {entry.split('=')[0]: entry.split('=')[1] for entry in entries if entry.find('=') != -1}
 
-print "No host series: ", bad_series
-#print list(hosts)
-print "Num hosts: ", len(hosts)
-#print list(hosts_amplified)
-print "Num hosts + amplified: ", len(hosts_amplified)
+    if series_items.has_key('hostname'):
+        hosts.add(series_items['hostname'])
+    else:
+        metrics_missing_hostname += 1
+
+    if series_items.has_key('amplifier'):
+        hosts_amplified[series_items['hostname']] += 1
+
+print('Found %d hosts' % len(hosts))
+print('Found %d amplified hosts' % len(hosts_amplified))
+print('Found %d total metrics with %d metrics missing hostnames. Making an average %f metrics per host' %
+      (total_metrics, metrics_missing_hostname, total_metrics/len(hosts)))
+
+print("\nAmplified host list:")
+for host in hosts_amplified.iterkeys():
+    print(host)
+
+print("\nHosts in aw1 but not in the amplified list.")
+for host in hosts:
+    if not hosts_amplified.has_key(host) and host.find('aw1') != -1:
+        print(host)
