2cba48fef08ea11bd62684e333c594cb6e063123
Tim Kuhlman <tim.kuhlman@hp.com>
2014-11-25 17:13:21 -0700

    Some more calculated stats

--- monasca_perf/influx_host_list.py
+++ monasca_perf/influx_host_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from collections import defaultdict
 import sys
 from influxdb import client as influxdb
 
@@ -10,7 +9,7 @@ hostname = sys.argv[3]
 db = influxdb.InfluxDBClient(hostname, 8086, username, password, 'mon')
 
 hosts = set()
-hosts_amplified = defaultdict(int)
+hosts_amplified = {}
 total_metrics = 0
 metrics_missing_hostname = 0
 for series in db.query('list series;')[0]["points"]:
@@ -23,17 +22,22 @@ for series in db.query('list series;')[0]["points"]:
     else:
         metrics_missing_hostname += 1
 
-    if series_items.has_key('amplifier'):
-        hosts_amplified[series_items['hostname']] += 1
+    if series_items.has_key('amplifier'):  # I assume if it has a amplifier it has a hostname also
+        hostname = series_items['hostname']
+        if not hosts_amplified.has_key(hostname) or hosts_amplified[hostname] < series_items['amplifier']:
+            hosts_amplified[hostname] = int(series_items['amplifier'])
 
 print('Found %d hosts' % len(hosts))
 print('Found %d amplified hosts' % len(hosts_amplified))
+amplification = hosts_amplified[hosts_amplified.keys()[0]]
+virtual_hosts = len(hosts) + (amplification * len(hosts_amplified))
+print('Total + amplified hosts = %d virtual hosts - caculated with amplification %d' % (virtual_hosts, amplification))
 print('Found %d total metrics with %d metrics missing hostnames. Making an average %f metrics per host' %
-      (total_metrics, metrics_missing_hostname, total_metrics/len(hosts)))
+      (total_metrics, metrics_missing_hostname, float(total_metrics)/virtual_hosts))
 
-print("\nAmplified host list:")
-for host in hosts_amplified.iterkeys():
-    print(host)
+#print("\nAmplified host list:")
+#for host in hosts_amplified.iterkeys():
+#    print(host)
 
 print("\nHosts in aw1 but not in the amplified list.")
 unamplified_count = 0
