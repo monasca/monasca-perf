2ff970c92e9adc37ab8d2495641829c2d0cd0161
Joe Keen <joe.keen@hp.com>
2015-03-31 12:26:17 -0600

    influxdb test now covers all the endpoints that talk to influx

--- monasca_perf/influxdb_functional.py
+++ monasca_perf/influxdb_functional.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
-import jsonschema
 
+import jsonschema
 from monascaclient import client
 from monascaclient import ksclient
 
@@ -43,10 +43,9 @@ def test_metric_list():
 
     schema = {'type': 'object',
               'properties':  {
-                'dimensions': {'type': 'object'},
-                'id': {'type': 'string'},
-                'name': {'type': 'string'}}}
-
+                  'dimensions': {'type': 'object'},
+                  'id': {'type': 'string'},
+                  'name': {'type': 'string'}}}
 
     for metric in result:
         jsonschema.validate(metric, schema)
@@ -70,18 +69,17 @@ def test_metric_statistics():
 
     schema = {'type': 'object',
               'properties': {
-                'dimensions': {'type': 'object'},
-                'statistics': {'type': 'array',
-                               'items': {'type': 'array',
-                                         'items': [{'type': 'string'},
-                                                   {'type': 'number',
-                                                    'minimum': 0}]}},
-                'id': {'type': 'string'},
-                'columns': {'type': 'array',
-                            'items': [{'type': 'string'},
-                                      {'type': 'string'}]},
-                'name': {'type': 'string'}}}
-
+                  'dimensions': {'type': 'object'},
+                  'statistics': {'type': 'array',
+                                 'items': {'type': 'array',
+                                           'items': [{'type': 'string'},
+                                                     {'type': 'number',
+                                                      'minimum': 0}]}},
+                  'id': {'type': 'string'},
+                  'columns': {'type': 'array',
+                              'items': [{'type': 'string'},
+                                        {'type': 'string'}]},
+                  'name': {'type': 'string'}}}
 
     for statistic in result:
         jsonschema.validate(statistic, schema)
@@ -115,31 +113,74 @@ def test_measurement_list():
                                         {'type': 'string'}]},
                   'name': {'type': 'string'}}}
 
-
     for measure in result:
         jsonschema.validate(measure, schema)
+
+
+def test_alarm_history_list():
+    alarms = mon_client("mini-mon",
+                        "password",
+                        auth_url,
+                        endpoint).alarms
+
+    result = alarms.history_list()
+
+    schema = {'type': 'object',
+              'properties': {
+                  'new_state': {'type': 'string'},
+                  'timestamp': {'type': 'string'},
+                  'metrics': {'type': 'array',
+                              'items': [{'type': 'object',
+                                         'properties': {
+                                             'dimensions': {'type': 'object'},
+                                             'id': {'type': 'null'},
+                                             'name': {'type': 'string'}}}]},
+                  'alarm_id': {'type': 'string'},
+                  'reason': {'type': 'string'},
+                  'reason_data': {'type': 'string'},
+                  'sub_alarms': {'type': 'array'},
+                  'old_state': {'type': 'string'},
+                  'id': {'type': 'string'}}}
+
+    for alarm in result:
+        jsonschema.validate(alarm, schema)
+
 
 def test_alarm_history():
     alarms = mon_client("mini-mon",
                         "password",
                         auth_url,
-                        endpoint).metrics
+                        endpoint).alarms
 
-    start = datetime.datetime.now()
-    ago = datetime.timedelta(minutes=2)
-    start = start - ago
+    result = alarms.history_list()
 
-    result = alarms.list_measurements(name="cpu.idle_perc",
-                                       start_time=start.isoformat(),
-                                       merge_metrics=True)
+    id = result[0]['alarm_id']
 
+    result = alarms.history(alarm_id=id)
 
+    schema = {'type': 'object',
+              'properties': {
+                  'new_state': {'type': 'string'},
+                  'timestamp': {'type': 'string'},
+                  'metrics': {'type': 'array',
+                              'items': [{'type': 'object',
+                                         'properties': {
+                                             'dimensions': {'type': 'object'},
+                                             'id': {'type': 'null'},
+                                             'name': {'type': 'string'}}}]},
+                  'alarm_id': {'type': 'string'},
+                  'reason': {'type': 'string'},
+                  'reason_data': {'type': 'string'},
+                  'sub_alarms': {'type': 'array'},
+                  'old_state': {'type': 'string'},
+                  'id': {'type': 'string'}}}
 
+    for alarm in result:
+        jsonschema.validate(alarm, schema)
 
 
 test_metric_list()
 test_metric_statistics()
 test_measurement_list()
-
-# alarm-history
-# alarm-history-list
+test_alarm_history_list()
+test_alarm_history()
