c3bb9cacb905ee5a30af4a66cfaad9d6681387ec
Allan Greentree <agreentree@hp.com>
2015-06-11 10:50:46 -0600

    added more vertica disk sizes to check

--- monasca_perf/disk.sh
+++ monasca_perf/disk.sh
@@ -5,6 +5,10 @@ do
         df -h | grep /dev/vda1
         du -h /var/kafka | tail -n1
         du -h /var/vertica | tail -n1
+        du -h /var/vertica/data | tail -n1
+        du -h /var/vertica/catalog | tail -n1
+        du -h /var/vertica/catalog/mon/v*/vertica.log | tail -n1
+        du -h /var/vertica/catalog/mon/v*/DataCollector | tail -n1
         du -h /var/log/kafka | tail -n1
         du -h /var/log/storm | tail -n1
         du -h /var/log/zookeeper | tail -n1
