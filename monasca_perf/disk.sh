555259a7984a10507e4b214a2606545d899f7d4b
Allan Greentree <agreentree@hp.com>
2015-09-03 14:34:19 -0600

    added alarm creation test for HOS

--- monasca_perf/disk.sh
+++ monasca_perf/disk.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# disk.sh
+# monitors disk usage
+# adapted for HOS environment 
+
 while :
 do
 	date
-        df -h | grep /dev/vda1
+        df -h | grep /dev/dm-0
         du -h /var/kafka | tail -n1
         du -h /var/vertica | tail -n1
         du -h /var/vertica/data | tail -n1
         du -h /var/vertica/catalog | tail -n1
         du -h /var/vertica/catalog/mon/v*/vertica.log | tail -n1
         du -h /var/vertica/catalog/mon/v*/DataCollector | tail -n1
+        du -h /var/lib/rabbitmq | tail -n1
         du -h /var/log/kafka | tail -n1
         du -h /var/log/storm | tail -n1
         du -h /var/log/zookeeper | tail -n1
