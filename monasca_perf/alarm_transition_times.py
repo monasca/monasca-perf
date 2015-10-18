0f28938ae5c4e4d5bfe4c2c86063c92f70743aab
Roland Hochmuth <roland.hochmuth@hp.com>
2015-10-18 09:08:08 -0600

    Added some comments and minor changes

--- monasca_perf/alarm_transition_times.py
+++ monasca_perf/alarm_transition_times.py
@@ -12,6 +12,9 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+# Measures the mean, median and standard of deviation for the amount of time
+# that it takes to transition alarms.
+
 import logging
 import random
 import time
@@ -21,7 +24,8 @@ from monascaclient import client as monasca_client
 
 import numpy as np
 
-AUTH_URL = 'http://192.168.10.5:35357/v3'
+#AUTH_URL = 'http://192.168.10.5:35357/v3'
+AUTH_URL = 'http://127.0.0.1:35357/v3'
 USERNAME = 'mini-mon'
 PASSWORD = 'password'
 DOMAIN_NAME = 'Default'
@@ -29,38 +33,56 @@ PROJECT_NAME = 'mini-mon'
 
 NUM_METRICS = 100
 METRIC_NAME = 'process_status'
-METRIC_LAG_TIME_SECS = 30
 
-ALARM_DEFINITION_NAME = 'process_status'
+ALARM_NAME = 'process_status'
 ALARM_EXPRESSION = 'max(process_status{}, 60) > 0'
 
-THRESHOLD_ENGINE_EVALUATION_PERIOD = 60
+THRESHOLD_ENGINE_EVALUATION_PERIOD_SECS = 60
+COLLECTION_PERIOD_SECS = 30
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('monasca-perf')
 logging.getLogger("requests").setLevel(logging.WARNING)
 
 def create_metric(id, value):
+    '''Creates a metric and returns it
+    :param id: ID of metric, which will be stored as a dimension
+    :param value: Value of metric
+    :return: JSON body for a metric
+    '''
     metric = {}
     metric['name'] = METRIC_NAME
     metric['value'] = value
-    metric['timestamp'] = (time.time() - METRIC_LAG_TIME_SECS)*1000
+    metric['timestamp'] = time.time()*1000
     metric['dimensions'] = {'id': id}
     return metric
 
 def send_metrics(client, metrics):
+    '''Send metrics to the Monasca API
+    :param client: Monasca client
+    :param metrics: JSON array or metrics
+    :return:
+    '''
     kwargs = {}
     kwargs['jsonbody'] = metrics
     client.metrics.create(**kwargs)
 
 def create_alarm_definition():
+    '''Create and alarm definition
+    :return:
+    '''
     defn = {}
-    defn['name'] = ALARM_DEFINITION_NAME
+    defn['name'] = ALARM_NAME
     defn['expression'] = ALARM_EXPRESSION
     defn['match_by'] = 'id'
     return defn
 
 def send_alarm_definition(client, defn):
+    '''Send alarm definition to Monasca API
+    :param client: Monasca client
+    :param defn: Alarm definition
+    :return:
+    '''
     try:
         client.alarm_definitions.create(**defn)
     except Exception as e:
@@ -69,12 +91,21 @@ def send_alarm_definition(client, defn):
         pass
 
 def get_alarms(client):
+    '''Get alarms with ALARM_NAME
+    :param client: Monasca client
+    :return: Array of alarms
+    '''
     kwargs = {}
-    kwargs['name'] = ALARM_DEFINITION_NAME
+    kwargs['name'] = ALARM_NAME
     response = client.alarms.list(**kwargs)
     return response
 
 def wait_for_all_alarms_to_transition(client, state):
+    '''Wait for all alarms to transition to the specified state
+    :param client: Monasca client
+    :param state: Alarm state. Either OK, ALARM or UNDETERMINED
+    :return:
+    '''
     all_transitioned = False
     while not all_transitioned:
         metrics = [create_metric(str(id), 0.0)
@@ -84,6 +115,17 @@ def wait_for_all_alarms_to_transition(client, state):
         transitioned = map(lambda x: x['state'] == state, alarms)
         all_transitioned = reduce(lambda x, y: x & y, transitioned)
         time.sleep(1)
+
+def get_metric_id(alarm):
+    '''Get metric ID for alarm
+    :param alarm: Alarm
+    :return: ID of metric used in alarm
+    '''
+    metrics = alarm['metrics']
+    metric = metrics[0]
+    dimensions = metric['dimensions']
+    id = int(dimensions['id'])
+    return id
 
 keystone_client = monasca_ksclient.KSClient(auth_url=AUTH_URL,
                                             username=USERNAME,
@@ -108,10 +150,11 @@ logger.info('wait for all alarms to transition to OK')
 wait_for_all_alarms_to_transition(monasca_client, 'OK')
 logger.info('done')
 
-# Wait for 120 seconds + a random time in the THRESHOLD_ENGINE_EVALUATION_PERIOD
+# Wait for 120 seconds + a random amount of time to give threshold engine time
+# to reach steady-state.
 logger.info('wait for 120 seconds + a random time between 0 and 59 seconds')
 start_time = time.time()
-wait_time = 120 + random.randint(0, THRESHOLD_ENGINE_EVALUATION_PERIOD-1)
+wait_time = 120 + random.randint(0, THRESHOLD_ENGINE_EVALUATION_PERIOD_SECS-1)
 while True:
     metrics = [create_metric(str(id), 0.0) for id in xrange(NUM_METRICS)]
     send_metrics(monasca_client, metrics)
@@ -123,29 +166,30 @@ logger.info('done')
 
 # Create random transition times for metrics in the THRESHOLD_ENGINE_EVALUATION_PERIOD
 start_time = time.time()
-transition_times = [start_time + random.randint(0, THRESHOLD_ENGINE_EVALUATION_PERIOD-1)
-                    for i in xrange(NUM_METRICS)]
-end_times = [None]*NUM_METRICS
-values = [0.0]*NUM_METRICS
+start_transition_times = [start_time + random.randint(0, THRESHOLD_ENGINE_EVALUATION_PERIOD_SECS-1)
+                          for i in xrange(NUM_METRICS)]
+end_transition_times = [None]*NUM_METRICS
+metric_values = [0.0]*NUM_METRICS
 
 # Transition metrics based on random transition times
 logger.info('randomly transition metrics and wait for all alarms to transition to ALARM')
 count = 0
+last_collection_time = time.time() - COLLECTION_PERIOD_SECS
 while True:
     current_time = time.time()
-    values = map(lambda x: 1.0 if current_time > x else 0.0, transition_times)
-    metrics = [create_metric(str(id), values[id]) for id in xrange(NUM_METRICS)]
-    send_metrics(monasca_client, metrics)
+    metric_values = map(lambda x: 1.0 if current_time >= x else 0.0, start_transition_times)
+    metrics = [create_metric(str(id), metric_values[id]) for id in xrange(NUM_METRICS)]
+
+    if current_time >= (last_collection_time + COLLECTION_PERIOD_SECS):
+        send_metrics(monasca_client, metrics)
+        last_collection_time = current_time
 
     alarms = get_alarms(monasca_client)
     for alarm in alarms:
         if alarm['state'] == 'ALARM':
-            metrics = alarm['metrics']
-            metric = metrics[0]
-            dimensions = metric['dimensions']
-            id = int(dimensions['id'])
-            if end_times[id] == None:
-                end_times[id] = time.time()
+            id = get_metric_id(alarm)
+            if end_transition_times[id] == None:
+                end_transition_times[id] = time.time()
 
     transitioned = map(lambda x: x['state'] == 'ALARM', alarms)
     all_transitioned = reduce(lambda x, y: x & y, transitioned)
@@ -158,11 +202,13 @@ while True:
     time.sleep(1)
 logger.info('done')
 
-elapsed_times = map(lambda x, y: x - y, end_times, transition_times)
+# Evaluate statistics
+elapsed_times = map(lambda x, y: x - y, end_transition_times, start_transition_times)
 mean = np.mean(elapsed_times)
 median = np.median(elapsed_times)
 std = np.std(elapsed_times)
 
+# Output statistics
 logger.info('mean: %f' % mean)
 logger.info('median: %f' % median)
 logger.info('std: %f' % std)
