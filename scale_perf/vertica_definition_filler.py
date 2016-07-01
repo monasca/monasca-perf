dc92fad06b93df69c7e4b86e7eb99bf4b9142eb8
Ryan Brandt <ryan.brandt@hp.com>
2016-07-01 09:44:46 -0600

    Create more accurate metrics when filling db

--- scale_perf/vertica_definition_filler.py
+++ scale_perf/vertica_definition_filler.py
@@ -13,11 +13,11 @@ import sys
 # Clear the current metrics from the DB for testing
 CLEAR_METRICS = True
 # Total definitions active at one time
-TOTAL_ACTIVE_DEFINITIONS = 8000
+TOTAL_ACTIVE_DEFINITIONS = 800
 # Number of new metric definitions per hour
-DEFINITIONS_PER_HOUR = 800
+NEW_VMS_PER_HOUR = 80
 # will fill x number days backwards from current day including current day
-NUMBER_OF_DAYS = 45
+NUMBER_OF_DAYS = 10
 
 CONN_INFO = {'user': 'dbadmin',
              'password': 'password'
@@ -31,6 +31,8 @@ BASE_DIMENSIONS = {"day": "{day}",
 
 # Tenant id to report metrics under
 TENANT_ID = "7e04ac703b024275aca4a9b3203847c8"
+# Region in which to report metrics
+REGION = "Region 1"
 
 
 
@@ -59,12 +61,108 @@ dims_list = []
 def_dims_list = []
 meas_list = []
 
-def_dims_temp = open(DEF_DIMS_FILENAME, 'w')
-def_temp = open(DEFINITIONS_FILENAME, 'w')
-dims_temp = open(DIMENSIIONS_FILENAME, 'w')
-meas_temp = open(MEASUREMENTS_FILENAME, 'w')
+next_resource_id = 1
 
 ID_SIZE = 20
+
+
+class vmSimulator(object):
+
+    def __init__(self, resource_id, tenant_id, region):
+        self.resource_id = resource_id
+        self.tenant_id = tenant_id or "tenant_1"
+        self.vm_tenant_id = "vm_tenant_1"
+        self.region = region or "region_1"
+        self.metrics = {}
+        self.base_dimensions = {
+            "cloud_name": "test_cloud",
+            "cluster": "test_cluster",
+            "service": "compute",
+            "resource_id": str(resource_id),
+            "zone": "nova",
+            "component": "vm",
+            "hostname": "test_vm_host_" + str(resource_id)
+        }
+        self.disks = ["sda", "sdb", "sdc", "sdd"]
+        self.network_devices = ['tap1']
+        self.metric_names = ["cpu.utilization_norm_perc",
+                             "cpu.utilization_perc",
+                             "disk.ephemeral.size",
+                             "disk.root.size",
+                             "host_alive_status",
+                             "instance",
+                             "io.errors_sec",
+                             "io.read_bytes_sec",
+                             "io.read_ops_sec",
+                             "io.write_bytes_sec",
+                             "io.write_ops_sec",
+                             "memory",
+                             "net.in_bytes",
+                             "net.in_bytes_sec",
+                             "net.in_packets",
+                             "net.in_packets_sec",
+                             "net.out_bytes",
+                             "net.out_bytes_sec",
+                             "net.out_packets",
+                             "net.out_packets_sec",
+                             "vcpus",
+                             "vm.cpu.time_ns",
+                             "vm.cpu.utilization_norm_perc",
+                             "vm.cpu.utilization_perc",
+                             "vm.host_alive_status",
+                             "vm.io.errors",
+                             "vm.io.errors_sec",
+                             "vm.io.errors_total",
+                             "vm.io.errors_total_sec",
+                             "vm.io.read_bytes",
+                             "vm.io.read_bytes_sec",
+                             "vm.io.read_bytes_total",
+                             "vm.io.read_bytes_total_sec",
+                             "vm.io.read_ops",
+                             "vm.io.read_ops_sec",
+                             "vm.io.read_ops_total",
+                             "vm.io.read_ops_total_sec",
+                             "vm.io.write_bytes",
+                             "vm.io.write_bytes_sec",
+                             "vm.io.write_bytes_total",
+                             "vm.io.write_bytes_total_sec",
+                             "vm.io.write_ops",
+                             "vm.io.write_ops_sec",
+                             "vm.io.write_ops_total",
+                             "vm.io.write_ops_total_sec",
+                             "vm.net.in_bytes",
+                             "vm.net.in_bytes_sec",
+                             "vm.net.in_packets",
+                             "vm.net.in_packets_sec",
+                             "vm.net.out_bytes",
+                             "vm.net.out_bytes_sec",
+                             "vm.net.out_packets",
+                             "vm.net.out_packets_sec"]
+
+    def create_metrics(self):
+        for name in self.metric_names:
+            dimensions = self.base_dimensions.copy()
+            if name.startswith('vm.'):
+                dimensions['tenant_id'] = self.vm_tenant_id
+            if 'disk' in name:
+                for disk in self.disks:
+                    dimensions['device'] = disk
+                    yield (name, dimensions)
+                continue
+
+            if 'io' in name and 'total' not in name:
+                for disk in self.disks:
+                    dimensions['device'] = disk
+                    yield (name, dimensions)
+                continue
+
+            if 'net' in name:
+                for device in self.network_devices:
+                    dimensions['device'] = device
+                    yield (name, dimensions)
+                continue
+
+            yield (name, dimensions)
 
 
 def add_measurement(def_dim_id, timestamp):
@@ -123,45 +221,65 @@ def set_dimension_values(active_dimensions, base_dimensions, day, hour, definiti
 
 def flush_data():
     global def_dims_list
+    def_dims_temp = open(DEF_DIMS_FILENAME, 'w')
     def_dims_temp.write('\n'.join(def_dims_list) + '\n')
+    def_dims_temp.close()
     def_dims_list = []
 
     global def_list
+    def_temp = open(DEFINITIONS_FILENAME, 'w')
     def_temp.write('\n'.join(def_list) + '\n')
+    def_temp.close()
     def_list = []
 
     global dims_list
+    dims_temp = open(DIMENSIIONS_FILENAME, 'w')
     dims_temp.write('\n'.join(dims_list) + '\n')
+    dims_temp.close()
     dims_list = []
 
     global meas_list
+    meas_temp = open(MEASUREMENTS_FILENAME, 'w')
     meas_temp.write('\n'.join(meas_list) + '\n')
+    meas_temp.close()
     meas_list = []
 
+    print("Writing to vertica")
+    query = COPY_QUERY.format(DEF_DIMS_FILENAME,
+                              DEFINITIONS_FILENAME,
+                              DIMENSIIONS_FILENAME,
+                              MEASUREMENTS_FILENAME)
+    run_query(query)
 
-def fill_metrics(number_of_days, definitions_per_hour):
+
+def fill_metrics(number_of_days, new_vms_per_hour):
     id_list = []
-    active_ids = []
+    active_vms = []
     for x in xrange(number_of_days):
         for y in xrange(24):
-            for z in xrange(definitions_per_hour):
-                active_dimensions = {}
-                set_dimension_values(active_dimensions, BASE_DIMENSIONS, day=str(x),
-                                     hour=str(y), definition=str(z))
-                active_dimensions["unique_key"] = str(x) + '-' + str(y) + '-' + str(z)
-                new_id = add_full_definition("api-test-0", active_dimensions,
-                                             tenant_id=TENANT_ID)
-                id_list.append(new_id)
-                active_ids.append(new_id)
+            for z in xrange(new_vms_per_hour):
 
-            if len(active_ids) > TOTAL_ACTIVE_DEFINITIONS:
-                active_ids = active_ids[DEFINITIONS_PER_HOUR:]
+                # active_dimensions = {}
+                # set_dimension_values(active_dimensions, BASE_DIMENSIONS, day=str(x),
+                #                      hour=str(y), definition=str(z))
+                # active_dimensions["unique_key"] = str(x) + '-' + str(y) + '-' + str(z)
+                # new_id = add_full_definition("api-test-0", active_dimensions,
+                #                              tenant_id=TENANT_ID)
+                global next_resource_id
+                active_vms.append(vmSimulator(resource_id=next_resource_id, tenant_id=TENANT_ID,
+                                              region=REGION))
+                next_resource_id += 1
 
-            for m_id in active_ids:
-                timestamp = datetime.datetime.utcnow() - datetime.timedelta(days=x, hours=y)
-                add_measurement(m_id, timestamp)
+            if len(active_vms) > TOTAL_ACTIVE_DEFINITIONS:
+                active_vms = active_vms[new_vms_per_hour:]
 
-            if len(def_dims_list) > TOTAL_ACTIVE_DEFINITIONS:
+            for vm in active_vms:
+                for metric in vm.create_metrics():
+                    new_id = add_full_definition(metric[0], metric[1], vm.tenant_id, vm.region)
+                    timestamp = datetime.datetime.utcnow() - datetime.timedelta(days=x, hours=y)
+                    add_measurement(new_id, timestamp)
+
+            if len(def_dims_list) > 1000000:
                 flush_data()
 
     flush_data()
@@ -190,18 +308,8 @@ def vertica_db_filler():
 
     # print("New definitions per day: {}".format(definitions_per_day))
     print("Creating metric history for the past {} days".format(NUMBER_OF_DAYS))
-    ids = fill_metrics(NUMBER_OF_DAYS, DEFINITIONS_PER_HOUR)
+    ids = fill_metrics(NUMBER_OF_DAYS, NEW_VMS_PER_HOUR)
     print("  Created {} definitions total".format(len(ids)))
-    def_dims_temp.close()
-    def_temp.close()
-    dims_temp.close()
-    meas_temp.close()
-    print("Writing to vertica")
-    query = COPY_QUERY.format(DEF_DIMS_FILENAME,
-                              DEFINITIONS_FILENAME,
-                              DIMENSIIONS_FILENAME,
-                              MEASUREMENTS_FILENAME)
-    run_query(query)
 
     print("Checking if data arrived...")
     print(" DefinitionDimensions")
@@ -213,18 +321,7 @@ def vertica_db_filler():
     print(" Measurements")
     print(run_query("SELECT count(*) FROM MonMetrics.Measurements;"))
 
-    print('Finished loading DB')
+    print('Finished loading VDB')
 
 if __name__ == "__main__":
-    return_code = 1
-    try:
-        vertica_db_filler()
-        return_code = 0
-    except Exception as e:
-        print(e)
-    finally:
-        def_dims_temp.close()
-        def_temp.close()
-        dims_temp.close()
-        meas_temp.close()
-        sys.exit(return_code)
+    sys.exit(vertica_db_filler())
