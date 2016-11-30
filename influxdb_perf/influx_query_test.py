aa1f90d84f55207b56cb3fcd00fcf68ed763c9d7
kaiyan-sheng <kaiyan.sheng@hp.com>
2016-11-30 10:01:14 -0700

    Add limit on all the queries

--- influxdb_perf/influx_query_test.py
+++ influxdb_perf/influx_query_test.py
@@ -2,8 +2,9 @@ import json
 import requests
 import time
 
-resource_id = '49e34c14-8efe-4c37-be24-f73e28436263'
-timestamp = '1476519218030'
+resource_id = '0a988066-810e-4b23-a159-c733938f8614'
+timestamp = '1476546163030'
+limit = 10000
 
 
 def main():
@@ -14,73 +15,76 @@ def main():
 
     # Query Metrics
     print("No filters query")
-    query = 'q=SHOW SERIES'
+    query = 'q=SHOW SERIES limit {}'.format(limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Name only ")
-    query = 'q=SHOW SERIES FROM "io.write_bytes_total_sec"'
+    query = 'q=SHOW SERIES FROM "io.write_bytes_total_sec" limit {}'.format(limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Name and start time")
-    query = 'q=SELECT * FROM "io.write_bytes_total_sec" where time >= {} limit 1'.format(timestamp)
+    query = 'q=SELECT * FROM "io.write_bytes_total_sec" where time >= {} limit {}'.format(
+        timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Name and resource_id (single result)")
     query = 'q=SHOW SERIES FROM "io.write_bytes_total_sec" WHERE ' \
-            'resource_id=\'{}\''.format(resource_id)
+            'resource_id=\'{}\' limit {}'.format(resource_id, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Name and max dimensions (max results)")
     query = 'q=SHOW SERIES FROM "io.write_bytes_total_sec" WHERE cloud_name=\'test_cloud\' and ' \
             'cluster=\'test_cluster\' and component=\'vm\' and hostname=\'test_1200\' and ' \
-            'lifespan=\'1200\' and service=\'compute\' and zone=\'nova\''
+            'lifespan=\'1200\' and service=\'compute\' and zone=\'nova\' limit {}'.format(limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Dimensions only, resource_id (single vm results)")
-    query = 'q=SHOW SERIES WHERE resource_id=\'{}\''.format(resource_id)
+    query = 'q=SHOW SERIES WHERE resource_id=\'{}\' limit {}'.format(resource_id, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Dimensions only, resource_id and device (single device result)")
     query = 'q=SHOW SERIES WHERE resource_id=\'{}\' and ' \
-            'device=\'vs1\''.format(resource_id)
+            'device=\'vs1\' limit {}'.format(resource_id, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-list | Dimensions only, device (one device over multiple vms result)")
-    query = 'q=SHOW SERIES WHERE device=\'vs1\''
+    query = 'q=SHOW SERIES WHERE device=\'vs1\' limit {}'.format(limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     # Query Measurements
     print("\nMeasurement-list | Name only merged (non-vm metric)")
-    query = 'q=SELECT * FROM "cpu.idle_perc" where time >= {}'.format(timestamp)
+    query = 'q=SELECT * FROM "cpu.time_ns" where time >= {} limit {}'.format(timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMeasurement-list | Name only grouped (non-vm metric)")
-    query = 'q=SELECT * FROM "cpu.idle_perc" where time >= {} group by *'.format(timestamp)
+    query = 'q=SELECT * FROM "cpu.time_ns" where time >= {} group by * limit {}'.format(
+        timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMeasurement-list | Name only merged (vm metric)")
-    query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {}'.format(timestamp)
+    query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {} limit {}'.format(timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMeasurement-list | Name only grouped (vm metric)")
-    query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {} group by *'.format(timestamp)
+    query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {} group by * limit {}'.format(
+        timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMeasurement-list | Name and resource_id (single result)")
     query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {0} and ' \
-            'resource_id=\'{1}\''.format(timestamp, resource_id)
+            'resource_id=\'{1}\' limit {2}'.format(timestamp, resource_id, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
@@ -88,24 +92,26 @@ def main():
     query = 'q=SELECT * FROM "vm.mem.used_mb" where time >= {} and ' \
             'cloud_name=\'test_cloud\' and cluster=\'test_cluster\' and component=\'vm\' and ' \
             'hostname=\'test_1200\' and lifespan=\'1200\' and service=\'compute\' and ' \
-            'zone=\'nova\''.format(timestamp)
+            'zone=\'nova\' limit {}'.format(timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     # Query Statistics
     print("\nMetric-statistics | name only merged (non-vm metric)")
-    query = 'q=SELECT max(value) from "cpu.time_ns" where time >= {}'.format(timestamp)
+    query = 'q=SELECT max(value) from "cpu.time_ns" where time >= {} limit {}'.format(
+        timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-statistics | name only merged (vm metric)")
-    query = 'q=SELECT max(value) from "vm.mem.free_perc" where time >= {}'.format(timestamp)
+    query = 'q=SELECT max(value) from "vm.mem.free_perc" where time >= {} limit {}'.format(
+        timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
     print("\nMetric-statistics | name and resource_id (single result)")
     query = 'q=SELECT max(value) from "vm.mem.free_perc" where time >= {} and ' \
-            'resource_id=\'{}\''.format(timestamp, resource_id)
+            'resource_id=\'{}\' limit {}'.format(timestamp, resource_id, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
@@ -113,7 +119,7 @@ def main():
     query = 'q=SELECT max(value) FROM "vm.mem.used_mb" where time >= {} and ' \
             'cloud_name=\'test_cloud\' and cluster=\'test_cluster\' and component=\'vm\' and ' \
             'hostname=\'test_1200\' and lifespan=\'1200\' and service=\'compute\' and ' \
-            'zone=\'nova\''.format(timestamp)
+            'zone=\'nova\' limit {}'.format(timestamp, limit)
     r, delta_time = run_query(query)
     status_output(r, delta_time)
 
